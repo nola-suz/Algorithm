ac53cd1809fd80ae426f9a609f2f888044725e3b
nola_suz <belljguis4@gmail.com>
2020-05-19 22:47:04 +0900

    solved abc155 a-c

---
+++ Atcoder/abc155/src/bin/c.rs
@@ -0,0 +1,72 @@
+#[warn(unused_mut)]
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let s = {
+            use std::io::Read;
+            let mut s = String::new();
+            std::io::stdin().read_to_string(&mut s).unwrap();
+            s
+        };
+        let mut iter = s.split_whitespace();
+        input_inner!{iter, $($r)*}
+    };
+}
+
+#[warn(unused_mut)]
+macro_rules! input_inner {
+    ($iter:expr) => {};
+    ($iter:expr, ) => {};
+
+    ($iter:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($iter, $t);
+        input_inner!{$iter $($r)*}
+    };
+}
+
+#[warn(unused_mut)]
+macro_rules! read_value {
+    ($iter:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($iter, $t)),* )
+    };
+
+    ($iter:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($iter, $t)).collect::<Vec<_>>()
+    };
+
+    ($iter:expr, chars) => {
+        read_value!($iter, String).chars().collect::<Vec<char>>()
+    };
+
+    ($iter:expr, usize1) => {
+        read_value!($iter, usize) - 1
+    };
+
+    ($iter:expr, $t:ty) => {
+        $iter.next().unwrap().parse::<$t>().expect("Parse error")
+    };
+}
+
+use std::collections::BTreeMap;
+
+fn main() {
+    input!{
+        n: usize,
+        s: [chars; n],
+    }
+
+    let mut map = BTreeMap::new();
+
+    for i in s.iter() {
+        *map.entry(i).or_insert(0) += 1;
+    }
+
+    let max = map.iter().max_by(|a, b| a.1.cmp(b.1)).unwrap().1;
+    let mut v = map.iter().filter(|a| a.1 == max).chain(iter::once(0));
+    for i in v.sorted() {
+        println!("{:?}", i);
+    }
+}
